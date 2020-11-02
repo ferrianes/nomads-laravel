d8c108ad47f27841ce77d38b541d8c34b38d3177
Ferrian Septiawan <ferrian27@gmail.com>
2020-11-02 15:25:28 +0700

    add auth admin

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,10 @@ Route::get('/checkout/success', 'CheckoutController@success')
 
 Route::prefix('admin')
     ->namespace('Admin')
+    ->middleware(['auth', 'admin'])
     ->group(function() {
         Route::get('/', 'DashboardController@index')
             ->name('dashboard');
     });
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');

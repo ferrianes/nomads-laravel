7185a1a987492d16be76be823eacfc823fa84947
Ferrian Septiawan <ferrian27@gmail.com>
2020-11-02 15:44:45 +0700

    delete construct home agar user bisa masuk ke home tanpa login

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -2,7 +2,7 @@
 <div class="container">
     <nav class="row navbar navbar-expand-lg navbar-light bg-white">
         <a href="{{ route('home') }}" class="navbar-brand">
-            <img src="frontend/images/logo_nomads.png" alt="logo Nomads">
+            <img src="{{ url('frontend/images/logo_nomads.png') }}" alt="logo Nomads">
         </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb">
             <span class="navbar-toggler-icon"></span>

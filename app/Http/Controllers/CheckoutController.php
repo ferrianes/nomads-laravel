dfd418d13981a465e5d28ac7f82cec1f63fd07de
Ferrian Septiawan <ferrian27@gmail.com>
2020-11-05 17:57:50 +0700

    add condition in checkout process

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -23,25 +23,33 @@ class CheckoutController extends Controller
 
     public function process(Request $request, $id)
     {
-        $travel_package = TravelPackage::findOrFail($id);
+        $transaction_exists = Transaction::where('users_id', Auth::user()->id)
+                                ->where('travel_packages_id', $id);
 
-        $transaction = Transaction::create([
-            'travel_packages_id' => $id,
-            'users_id' => Auth::user()->id,
-            'additional_visa' => 0,
-            'transaction_total' => $travel_package->price,
-            'transaction_status' => 'IN_CART'
-        ]);
+        if ($transaction_exists->exists()) {
+            return redirect()->route('checkout', $transaction_exists->first()->id);
+        } else {
+            $travel_package = TravelPackage::findOrFail($id);
+            
+            $transaction = Transaction::create([
+                'travel_packages_id' => $id,
+                'users_id' => Auth::user()->id,
+                'additional_visa' => 0,
+                'transaction_total' => $travel_package->price,
+                'transaction_status' => 'IN_CART'
+            ]);
+    
+            TransactionDetail::create([
+                'transactions_id' => $transaction->id,
+                'username' => Auth::user()->username,
+                'nationality' => Auth::user()->nationality,
+                'is_visa' => Auth::user()->is_visa,
+                'doe_passport' => Auth::user()->doe_passport
+            ]);
+    
+            return redirect()->route('checkout', $transaction->id);
+        }
 
-        TransactionDetail::create([
-            'transactions_id' => $transaction->id,
-            'username' => Auth::user()->username,
-            'nationality' => Auth::user()->nationality,
-            'is_visa' => Auth::user()->is_visa,
-            'doe_passport' => Auth::user()->doe_passport
-        ]);
-
-        return redirect()->route('checkout', $transaction->id);
     }
 
     public function remove(Request $request, $detail_id)
